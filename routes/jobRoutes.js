8c6117545d6528dc87ea0f86f972f445bf1e5905
Sayed Razibur Rahman <sayedrazibur@gmail.com>
2025-04-24 12:27:24 +0600

    update

---
+++ routes/jobRoutes.js
@@ -0,0 +1,28 @@
+import express from "express";
+import verifyRole from "../middleware/AdminRole.js";
+import {
+  createJob,
+  deleteJob,
+  fetchJobs,
+  showJob,
+  updateJob,
+} from "../controllers/jobController.js";
+
+const jobRoute = express.Router();
+
+// Public route to fetch all jobs (no auth)
+jobRoute.get("/all", fetchJobs);
+
+// Admin-only route to create a job
+jobRoute.post("/create", verifyRole("Admin", "Modarator"), createJob);
+
+// Public route to view a job by ID
+jobRoute.get("/:id", showJob);
+
+// Admin-only route to update a job
+jobRoute.put("/update/:id", verifyRole("Admin", "Modarator"), updateJob);
+
+// Admin-only route to delete a job
+jobRoute.delete("/delete/:id", verifyRole("Admin", "Modarator"), deleteJob);
+
+export default jobRoute;
