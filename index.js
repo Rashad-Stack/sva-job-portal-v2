50505af4049d788ee7a1642ca7552c28c12ccc63
Rashad-Stack <tex.eng.rashed@gmail.com>
2025-05-18 02:59:41 +0600

    Remove authentication middleware from application routes for public access

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.use("/api/v2/category", categoryRoute);
 app.use("/api/v2/status", authenticateUser, statusRoutes);
 app.use("/api/v2/job-index", authenticateUser, jobIndexRouter);
 app.use("/api/v2/job-index/changelog", authenticateUser, changeLogRoute);
-app.use("/api/v2/job/application", authenticateUser, applicationRouter);
+app.use("/api/v2/job/application", applicationRouter);
 app.use("/api/v2/job/forms", formRouter);
 
 // Start server
